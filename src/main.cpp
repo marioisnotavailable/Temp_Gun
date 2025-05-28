edcc9905aeac998a1fc30d970a61a3ff77b724c3
Mirko <mario.haunold07@gmail.com>
2025-05-28 15:30:26 +0200

    Refactor code style for consistency: adjust spacing and formatting in function declarations and control structures

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 void initWiFi();
 void connectToWiFi(const String &ssid, const String &password);
 void initMQTT();
-void mqttCallback(char* topic, byte* payload, unsigned int length);
+void mqttCallback(char *topic, byte *payload, unsigned int length);
 void otaTask(void *parameter);
 void Batterie();
 void deepsleep();
@@ -20,7 +20,8 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     deepsleep();
@@ -31,31 +32,34 @@ void setup() {
     analogSetAttenuation(ADC_11db);
 
     xTaskCreate(
-        otaTask,          // Task function
-        "OTA Task",      // Name of the task
-        4096,             // Stack size (in words)
-        NULL,             // Task input parameter
-        1,                // Priority of the task
-        &otaTaskHandle    // Task handle
+        otaTask,       // Task function
+        "OTA Task",    // Name of the task
+        4096,          // Stack size (in words)
+        NULL,          // Task input parameter
+        1,             // Priority of the task
+        &otaTaskHandle // Task handle
     );
 
     Wire.begin(15, 2); // Initialize I2C with GPIO 15 (SDA) and GPIO 2 (SCL)
 
     // Initialize MLX90614 sensor
-    if (!mlx.begin()) {
+    if (!mlx.begin())
+    {
         Serial.printf("Error initializing MLX90614 sensor!\n");
     }
     Serial.printf("MLX90614 sensor initialized.\n");
 
-    //initMQTT(); // Initialize MQTT connection
+    // initMQTT(); // Initialize MQTT connection
 }
 
 unsigned long lastTempReadTime = 0;
 
-void loop() {
+void loop()
+{
     // Non-blocking temperature data read
     unsigned long currentMillis = millis();
-    if (currentMillis - lastTempReadTime >= 100) {
+    if (currentMillis - lastTempReadTime >= 100)
+    {
         lastTempReadTime = currentMillis;
 
         float ambientTemp = mlx.readAmbientTempC();
@@ -63,10 +67,11 @@ void loop() {
         String ambientTempStr = String(ambientTemp).c_str();
         String objectTempStr = String(objectTemp).c_str();
 
-        Serial.printf("Ambient: %s C, Object: %s C\n", ambientTempStr.c_str(),objectTempStr.c_str());
+        Serial.printf("Ambient: %s C, Object: %s C\n", ambientTempStr.c_str(), objectTempStr.c_str());
 
         // Publish sensor values to separate MQTT topics
-        if (mqttClient.connected()) {
+        if (mqttClient.connected())
+        {
             mqttClient.publish("mario/tempgun/temperature/ambient", ambientTempStr.c_str());
             mqttClient.publish("mario/tempgun/temperature/object", objectTempStr.c_str());
             Serial.printf("Published to MQTT: ambient=%s, object=%s\n", ambientTempStr.c_str(), objectTempStr.c_str());
@@ -75,27 +80,31 @@ void loop() {
 
     mqttClient.loop(); // Ensure MQTT client stays connected and processes incoming messages
 
-    Batterie();// Read battery voltage on sensor VP (GPIO 36) with 1:1 voltage divider
+    Batterie(); // Read battery voltage on sensor VP (GPIO 36) with 1:1 voltage divider
 }
 
-void connectToWiFi(const String &ssid, const String &password) {
+void connectToWiFi(const String &ssid, const String &password)
+{
     int connectionTries = 0;
     WiFi.begin(ssid, password);
     Serial.printf("Connecting to WiFi: %s\n", ssid.c_str());
 
-    while (WiFi.status() != WL_CONNECTED && connectionTries < 10) {
+    while (WiFi.status() != WL_CONNECTED && connectionTries < 10)
+    {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         Serial.print(".");
         connectionTries++;
     }
     Serial.println();
-    if (WiFi.status() == WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
         Serial.printf("Connected to WiFi: %s\n", ssid.c_str());
         Serial.printf("IP Address: %s\n", WiFi.localIP().toString().c_str());
     }
 }
 
-void initWiFi() {
+void initWiFi()
+{
     preferences.begin("wificonfig", false);
 
     String ssid1 = preferences.getString("SSID1");
@@ -107,18 +116,21 @@ void initWiFi() {
 
     connectToWiFi(ssid1, password1);
 
-    if (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.status() != WL_CONNECTED)
+    {
         connectToWiFi(ssid2, password2);
     }
 
-    if (WiFi.status() != WL_CONNECTED) {
+    if (WiFi.status() != WL_CONNECTED)
+    {
         Serial.println("Failed to connect to any WiFi network. Restarting...");
         ESP.restart();
     }
- // Close Preferences after use
+    // Close Preferences after use
 }
 
-void initMQTT() {
+void initMQTT()
+{
     preferences.begin("mqttconfig", false);
 
     String MqttServer = preferences.getString("MQTT_SERVER");
@@ -131,12 +143,16 @@ void initMQTT() {
     mqttClient.setServer(MqttServer.c_str(), MqttPort);
     mqttClient.setCallback(mqttCallback); // Set the callback function for incoming messages
 
-    while (!mqttClient.connected()) {
+    while (!mqttClient.connected())
+    {
         Serial.println("Connecting to MQTT...");
-        if (mqttClient.connect("ESP32Client", MqttUser.c_str(), MqttPassword.c_str())) {
+        if (mqttClient.connect("ESP32Client", MqttUser.c_str(), MqttPassword.c_str()))
+        {
             Serial.println("Connected to MQTT broker");
-            //mqttClient.subscribe("sensor/commands"); // Example subscription
-        } else {
+            // mqttClient.subscribe("sensor/commands"); // Example subscription
+        }
+        else
+        {
             Serial.print("Failed to connect to MQTT. State: ");
             Serial.println(mqttClient.state());
             vTaskDelay(2000 / portTICK_PERIOD_MS);
@@ -144,40 +160,54 @@ void initMQTT() {
     }
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
+void mqttCallback(char *topic, byte *payload, unsigned int length)
+{
     Serial.print("Message arrived on topic: ");
     Serial.println(topic);
     Serial.print("Message: ");
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         Serial.print((char)payload[i]);
     }
 }
 
-void otaTask(void *parameter) {
+void otaTask(void *parameter)
+{
     ArduinoOTA.begin();
-    for (;;) {
+    for (;;)
+    {
         ArduinoOTA.handle();
-        //Serial.println("OTA handle");
+        // Serial.println("OTA handle");
         vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
     }
 }
 
-void Batterie(){
+void Batterie()
+{
     static int raw = 0;
     raw += analogRead(36);
     static int count = 0;
-    if(count++ > 1000) {
+    if (count++ > 1000)
+    {
         float voltage = (raw / 1000 / 4095.0) * 3.3 * 2.0;
         Serial.printf("Batterie = %.3f V (raw=%d)\n", voltage, raw);
         raw = 0;
         count = 0;
+        if (mqttClient.connected())
+        {
+            String batteryVoltage = String(voltage, 3);
+            mqttClient.publish("mario/tempgun/battery", batteryVoltage.c_str());
+            Serial.printf("Published battery voltage to MQTT: %s V\n", batteryVoltage.c_str());
+        }
     }
 }
 
-void deepsleep() {
+void deepsleep()
+{
     preferences.begin("deepsleep", false);
 
-    if (preferences.getBool("DEEPSLEEP", false)) {
+    if (preferences.getBool("DEEPSLEEP", false))
+    {
         preferences.putBool("DEEPSLEEP", false);
         Serial.printf("Entering deep sleep mode with no wakeup...\n");
         preferences.end();
